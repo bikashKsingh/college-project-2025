88ce8c8787d9272c60e20560af4240e8a3031517
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2025-04-11 09:22:43 +0530

    variable concept clear

---
+++ JavaScript/hello.js
@@ -0,0 +1,49 @@
+// console.log("Hello World!");
+
+var num;
+let number = undefined;
+const age = 20;
+
+let n, a, b, c;
+n = 10;
+// console.log(typeof 2);
+// console.log(typeof num);
+// console.log(typeof number);
+
+let marks = 20;
+let marks2 = 21;
+
+let result = marks + marks2;
+// console.log(result);
+
+// Operators HW
+
+// conditional statement
+if ("") {
+  console.log("Age is greater than 20");
+} else {
+  console.log("Age is either 20 or below");
+}
+
+let array = [];
+if (array) {
+  console.log("Non Empty Array");
+} else {
+  console.log("Empty Array");
+}
+
+// Falsy values
+// false
+// 0
+// -0
+// ""
+// null
+// undefined
+// NaN
+
+// Thruthy values
+// true
+// all number except 0
+// " "
+// []
+// {}
